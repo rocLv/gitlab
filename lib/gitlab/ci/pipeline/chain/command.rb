45fbddeadfca0e7549bdefcf00fac26fd170ac32
David Fernandez <dfernandez@gitlab.com>
2021-05-07 17:21:23 +0200

    Step 1: Pipeline creation
    * Add a `Packages::Push` model to link to a package file and have a
    `sha`
    * Create 'Packages::CreatePipelineService' that will create a pipeline
    out of a push
    * Update pipeline enums for a new event: package_push
    * Update pipeline chain to accept the package push
       * a package page is *not* a reference to a specific sha of a git
         repository

--- lib/gitlab/ci/pipeline/chain/command.rb
+++ lib/gitlab/ci/pipeline/chain/command.rb
@@ -12,7 +12,7 @@ module Gitlab
           :seeds_block, :variables_attributes, :push_options,
           :chat_data, :allow_mirror_update, :bridge, :content, :dry_run,
           # These attributes are set by Chains during processing:
-          :config_content, :yaml_processor_result, :workflow_rules_result, :pipeline_seed
+          :config_content, :yaml_processor_result, :workflow_rules_result, :pipeline_seed, :package_push
         ) do
           include Gitlab::Utils::StrongMemoize
 
@@ -51,6 +51,8 @@ module Gitlab
 
           def sha
             strong_memoize(:sha) do
+              break package_push.sha if package_push
+
               project.commit(origin_sha || origin_ref).try(:id)
             end
           end
