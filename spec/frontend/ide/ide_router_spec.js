5c54c2565bb95451291a40cce740f4466dd1bf98
Denys Mishunov <dmishunov@gitlab.com>
2021-07-13 10:19:05 +0200

    Supply project data to integration tests
    Updated router spec
    
    Fixed the ESLint job

--- spec/frontend/ide/ide_router_spec.js
+++ spec/frontend/ide/ide_router_spec.js
@@ -5,6 +5,8 @@ import { createStore } from '~/ide/stores';
 describe('IDE router', () => {
   const PROJECT_NAMESPACE = 'my-group/sub-group';
   const PROJECT_NAME = 'my-project';
+  const PROJECT_NAMESPACED = `${PROJECT_NAMESPACE}/${PROJECT_NAME}`;
+  const DEFAULT_BRANCH_ID = 'main';
   const TEST_PATH = `/project/${PROJECT_NAMESPACE}/${PROJECT_NAME}/merge_requests/2`;
 
   let store;
@@ -17,29 +19,56 @@ describe('IDE router', () => {
     jest.spyOn(store, 'dispatch').mockReturnValue(new Promise(() => {}));
   });
 
+  it.each`
+    route                                                                                | branchId                   | basePath
+    ${`/project/${PROJECT_NAMESPACED}/tree/${DEFAULT_BRANCH_ID}/-/src/blob/`}            | ${DEFAULT_BRANCH_ID}       | ${'src/blob/'}
+    ${`/project/${PROJECT_NAMESPACED}/tree/${DEFAULT_BRANCH_ID}/-/src/blob`}             | ${DEFAULT_BRANCH_ID}       | ${'src/blob'}
+    ${`/project/${PROJECT_NAMESPACED}/tree/blob/-/src/blob`}                             | ${undefined}               | ${'src/blob'}
+    ${`/project/${PROJECT_NAMESPACED}/tree/${DEFAULT_BRANCH_ID}/-/src/tree/`}            | ${DEFAULT_BRANCH_ID}       | ${'src/tree/'}
+    ${`/project/${PROJECT_NAMESPACED}/tree/weird:branch/name-123/-/src/tree/`}           | ${'weird:branch/name-123'} | ${'src/tree/'}
+    ${`/project/${PROJECT_NAMESPACED}/blob/${DEFAULT_BRANCH_ID}/-/src/blob`}             | ${DEFAULT_BRANCH_ID}       | ${'src/blob'}
+    ${`/project/${PROJECT_NAMESPACED}/blob/${DEFAULT_BRANCH_ID}/-/src/edit`}             | ${DEFAULT_BRANCH_ID}       | ${'src/edit'}
+    ${`/project/${PROJECT_NAMESPACED}/blob/${DEFAULT_BRANCH_ID}/-/src/merge_requests/2`} | ${DEFAULT_BRANCH_ID}       | ${'src/merge_requests/2'}
+    ${`/project/${PROJECT_NAMESPACED}/blob/blob/-/src/blob`}                             | ${undefined}               | ${'src/blob'}
+    ${`/project/${PROJECT_NAMESPACED}/edit/blob/-/src/blob`}                             | ${undefined}               | ${'src/blob'}
+    ${`/project/${PROJECT_NAMESPACED}/tree/blob`}                                        | ${undefined}               | ${undefined}
+    ${`/project/${PROJECT_NAMESPACED}/edit`}                                             | ${undefined}               | ${undefined}
+    ${`/project/${PROJECT_NAMESPACED}`}                                                  | ${undefined}               | ${''}
+  `('opens correct branch if supplied when route is "$route"', ({ route, branchId, basePath }) => {
+    router.push(route);
+
+    if (branchId) {
+      expect(store.dispatch).toHaveBeenCalledWith('openBranch', {
+        projectId: PROJECT_NAMESPACED,
+        branchId,
+        basePath,
+      });
+    }
+    expect(store.dispatch).not.toHaveBeenCalledWith('openMergeRequest');
+  });
+
+  it('opens MR for correct URL schema', () => {
+    const mergeRequestId = '2';
+    const route = `/project/${PROJECT_NAMESPACED}/merge_requests/${mergeRequestId}`;
+    router.push(route);
+
+    expect(store.dispatch).toHaveBeenCalledWith('openMergeRequest', {
+      projectId: `${PROJECT_NAMESPACE}/${PROJECT_NAME}`,
+      mergeRequestId,
+      targetProjectId: undefined,
+    });
+    expect(store.dispatch).not.toHaveBeenCalledWith('openBranch');
+  });
+
   [
-    `/project/${PROJECT_NAMESPACE}/${PROJECT_NAME}/tree/main/-/src/blob/`,
-    `/project/${PROJECT_NAMESPACE}/${PROJECT_NAME}/tree/main/-/src/blob`,
-    `/project/${PROJECT_NAMESPACE}/${PROJECT_NAME}/tree/blob/-/src/blob`,
-    `/project/${PROJECT_NAMESPACE}/${PROJECT_NAME}/tree/main/-/src/tree/`,
-    `/project/${PROJECT_NAMESPACE}/${PROJECT_NAME}/tree/weird:branch/name-123/-/src/tree/`,
-    `/project/${PROJECT_NAMESPACE}/${PROJECT_NAME}/blob/main/-/src/blob`,
-    `/project/${PROJECT_NAMESPACE}/${PROJECT_NAME}/blob/main/-/src/edit`,
-    `/project/${PROJECT_NAMESPACE}/${PROJECT_NAME}/blob/main/-/src/merge_requests/2`,
-    `/project/${PROJECT_NAMESPACE}/${PROJECT_NAME}/blob/blob/-/src/blob`,
-    `/project/${PROJECT_NAMESPACE}/${PROJECT_NAME}/edit/blob/-/src/blob`,
-    `/project/${PROJECT_NAMESPACE}/${PROJECT_NAME}/merge_requests/2`,
-    `/project/${PROJECT_NAMESPACE}/${PROJECT_NAME}/tree/blob`,
-    `/project/${PROJECT_NAMESPACE}/${PROJECT_NAME}/edit`,
-    `/project/${PROJECT_NAMESPACE}/${PROJECT_NAME}`,
+    `/project/${PROJECT_NAMESPACE}/${PROJECT_NAME}/merge_requests/`,
+    `/project/${PROJECT_NAMESPACE}/${PROJECT_NAME}/merge_requests`,
+    `/project/${PROJECT_NAMESPACE}/${PROJECT_NAME}/merge_requests/2/3`,
   ].forEach((route) => {
-    it(`finds project path when route is "${route}"`, () => {
+    it(`neither opens an MR nor branch for "${route}"`, () => {
       router.push(route);
 
-      expect(store.dispatch).toHaveBeenCalledWith('getProjectData', {
-        namespace: PROJECT_NAMESPACE,
-        projectId: PROJECT_NAME,
-      });
+      expect(store.dispatch).not.toHaveBeenCalledWith('openMergeRequest');
     });
   });
 
