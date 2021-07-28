41ba8b225104a91eb29f3287d614e73d4cac8057
Pedro Pombeiro <noreply@pedro.pombei.ro>
2021-07-28 17:50:48 +0200

    Fix GroupRunnersResolver specs

--- spec/graphql/resolvers/ci/group_runners_resolver_spec.rb
+++ spec/graphql/resolvers/ci/group_runners_resolver_spec.rb
@@ -7,8 +7,6 @@ RSpec.describe Resolvers::Ci::GroupRunnersResolver do
 
   include_context 'runners resolver setup'
 
-  it_behaves_like Resolvers::Ci::RunnersResolver
-
   shared_context 'resolve args with membership' do
     let(:membership) { nil }
     let(:args) do
@@ -17,6 +15,8 @@ RSpec.describe Resolvers::Ci::GroupRunnersResolver do
   end
 
   describe '#resolve' do
+    let(:args) { }
+
     subject { resolve(described_class, obj: obj, ctx: { current_user: user }, args: args).items.to_a }
 
     context 'with user as group owner' do
@@ -64,8 +64,182 @@ RSpec.describe Resolvers::Ci::GroupRunnersResolver do
         end
       end
 
+      context 'with obj set to unsupported value' do
+        let_it_be(:obj) { build(:project) }
+
+        it 'raises error' do
+          expect { subject }.to raise_error('Expected group missing')
+        end
+      end
+
       context 'with obj set to group' do
         let(:obj) { group }
+
+        context 'with empty args' do
+          let(:args) do
+            {}
+          end
+
+          context 'when the user cannot see runners' do
+            let(:user) { build(:user) }
+
+            it 'returns no runners' do
+              is_expected.to be_empty
+            end
+          end
+
+          context 'without sort' do
+            it 'returns all the runners' do
+              is_expected.to contain_exactly(inactive_project_runner, offline_project_runner, group_runner, subgroup_runner)
+            end
+          end
+        end
+
+        context 'with a sort argument' do
+          context "set to :contacted_asc" do
+            let(:args) do
+              { sort: :contacted_asc }
+            end
+
+            it { is_expected.to eq([offline_project_runner, inactive_project_runner, group_runner, subgroup_runner]) }
+          end
+
+          context "set to :contacted_desc" do
+            let(:args) do
+              { sort: :contacted_desc }
+            end
+
+            it { is_expected.to eq([offline_project_runner, inactive_project_runner, group_runner, subgroup_runner].reverse) }
+          end
+
+          context "set to :created_at_desc" do
+            let(:args) do
+              { sort: :created_at_desc }
+            end
+
+            it { is_expected.to eq([subgroup_runner, group_runner, offline_project_runner, inactive_project_runner]) }
+          end
+
+          context "set to :created_at_asc" do
+            let(:args) do
+              { sort: :created_at_asc }
+            end
+
+            it { is_expected.to eq([subgroup_runner, group_runner, offline_project_runner, inactive_project_runner].reverse) }
+          end
+        end
+
+        context 'when type is filtered' do
+          let(:args) do
+            { type: runner_type.to_s }
+          end
+
+          context 'to instance runners' do
+            let(:runner_type) { :instance_type }
+
+            it 'returns empty array' do
+              is_expected.to eq([])
+            end
+          end
+
+          context 'to group runners' do
+            let(:runner_type) { :group_type }
+
+            it 'returns the group runner' do
+              is_expected.to contain_exactly(group_runner, subgroup_runner)
+            end
+          end
+
+          context 'to project runners' do
+            let(:runner_type) { :project_type }
+
+            it 'returns the project runner' do
+              is_expected.to contain_exactly(inactive_project_runner, offline_project_runner)
+            end
+          end
+        end
+
+        context 'when status is filtered' do
+          let(:args) do
+            { status: runner_status.to_s }
+          end
+
+          context 'to active runners' do
+            let(:runner_status) { :active }
+
+            it 'returns the instance and group runners' do
+              is_expected.to contain_exactly(offline_project_runner, group_runner, subgroup_runner)
+            end
+          end
+
+          context 'to offline runners' do
+            let(:runner_status) { :offline }
+
+            it 'returns the offline project runner' do
+              is_expected.to contain_exactly(offline_project_runner)
+            end
+          end
+        end
+
+        context 'when tag list is filtered' do
+          let(:args) do
+            { tag_list: tag_list }
+          end
+
+          context 'with "project_runner" tag' do
+            let(:tag_list) { ['project_runner'] }
+
+            it 'returns the project_runner runners' do
+              is_expected.to contain_exactly(offline_project_runner, inactive_project_runner)
+            end
+          end
+
+          context 'with "project_runner" and "active_runner" tags as comma-separated string' do
+            let(:tag_list) { ['project_runner,active_runner'] }
+
+            it 'returns the offline_project_runner runner' do
+              is_expected.to contain_exactly(offline_project_runner)
+            end
+          end
+
+          context 'with "active_runner" and "project_runner" tags as array' do
+            let(:tag_list) { %w[project_runner active_runner] }
+
+            it 'returns the offline_project_runner runner' do
+              is_expected.to contain_exactly(offline_project_runner)
+            end
+          end
+        end
+
+        context 'when text is filtered' do
+          let(:args) do
+            { search: search_term }
+          end
+
+          context 'to "project"' do
+            let(:search_term) { 'project' }
+
+            it 'returns both project runners' do
+              is_expected.to contain_exactly(inactive_project_runner, offline_project_runner)
+            end
+          end
+
+          context 'to "group"' do
+            let(:search_term) { 'group' }
+
+            it 'returns group runners' do
+              is_expected.to contain_exactly(group_runner, subgroup_runner)
+            end
+          end
+
+          context 'to "defghi"' do
+            let(:search_term) { 'defghi' }
+
+            it 'returns runners containing term in token' do
+              is_expected.to contain_exactly(offline_project_runner)
+            end
+          end
+        end
 
         context 'with a membership argument' do
           include_context 'resolve args with membership'
--- spec/graphql/resolvers/ci/runners_resolver_spec.rb
+++ spec/graphql/resolvers/ci/runners_resolver_spec.rb
@@ -7,24 +7,182 @@ RSpec.describe Resolvers::Ci::RunnersResolver do
 
   include_context 'runners resolver setup'
 
-  it_behaves_like Resolvers::Ci::RunnersResolver
-
   describe '#resolve' do
-    subject { resolve(described_class, obj: obj, ctx: { current_user: user }, args: {}) }
+    let(:obj) { nil }
 
-    context 'with obj set to nil value' do
-      let(:obj) { nil }
+    subject { resolve(described_class, obj: obj, ctx: { current_user: user }, args: args).items.to_a }
 
-      it 'does not raise an error' do
-        expect { subject }.not_to raise_error
+    context 'with empty args' do
+      let(:args) do
+        {}
+      end
+
+      context 'with obj set to unsupported value' do
+        let_it_be(:obj) { build(:group) }
+
+        it 'raises error' do
+          expect { subject }.to raise_error("Unexpected parent type: #{obj.class}")
+        end
+      end
+
+      context 'when the user cannot see runners' do
+        let(:user) { build(:user) }
+
+        it 'returns no runners' do
+          is_expected.to be_empty
+        end
+      end
+
+      context 'without sort' do
+        it 'returns all the runners' do
+          is_expected.to contain_exactly(inactive_project_runner, offline_project_runner, group_runner, subgroup_runner, instance_runner)
+        end
       end
     end
 
-    context 'with obj set to unsupported value' do
-      let_it_be(:obj) { build(:group) }
+    context 'with a sort argument' do
+      context "set to :contacted_asc" do
+        let(:args) do
+          { sort: :contacted_asc }
+        end
 
-      it 'returns no runners' do
-        expect { subject }.to raise_error("Unexpected parent type: #{obj.class}")
+        it { is_expected.to eq([offline_project_runner, instance_runner, inactive_project_runner, group_runner, subgroup_runner]) }
+      end
+
+      context "set to :contacted_desc" do
+        let(:args) do
+          { sort: :contacted_desc }
+        end
+
+        it { is_expected.to eq([offline_project_runner, instance_runner, inactive_project_runner, group_runner, subgroup_runner].reverse) }
+      end
+
+      context "set to :created_at_desc" do
+        let(:args) do
+          { sort: :created_at_desc }
+        end
+
+        it { is_expected.to eq([instance_runner, subgroup_runner, group_runner, offline_project_runner, inactive_project_runner]) }
+      end
+
+      context "set to :created_at_asc" do
+        let(:args) do
+          { sort: :created_at_asc }
+        end
+
+        it { is_expected.to eq([instance_runner, subgroup_runner, group_runner, offline_project_runner, inactive_project_runner].reverse) }
+      end
+    end
+
+    context 'when type is filtered' do
+      let(:args) do
+        { type: runner_type.to_s }
+      end
+
+      context 'to instance runners' do
+        let(:runner_type) { :instance_type }
+
+        it 'returns the instance runner' do
+          is_expected.to contain_exactly(instance_runner)
+        end
+      end
+
+      context 'to group runners' do
+        let(:runner_type) { :group_type }
+
+        it 'returns the group runner' do
+          is_expected.to contain_exactly(group_runner, subgroup_runner)
+        end
+      end
+
+      context 'to project runners' do
+        let(:runner_type) { :project_type }
+
+        it 'returns the project runner' do
+          is_expected.to contain_exactly(inactive_project_runner, offline_project_runner)
+        end
+      end
+    end
+
+    context 'when status is filtered' do
+      let(:args) do
+        { status: runner_status.to_s }
+      end
+
+      context 'to active runners' do
+        let(:runner_status) { :active }
+
+        it 'returns the instance and group runners' do
+          is_expected.to contain_exactly(offline_project_runner, group_runner, subgroup_runner, instance_runner)
+        end
+      end
+
+      context 'to offline runners' do
+        let(:runner_status) { :offline }
+
+        it 'returns the offline project runner' do
+          is_expected.to contain_exactly(offline_project_runner)
+        end
+      end
+    end
+
+    context 'when tag list is filtered' do
+      let(:args) do
+        { tag_list: tag_list }
+      end
+
+      context 'with "project_runner" tag' do
+        let(:tag_list) { ['project_runner'] }
+
+        it 'returns the project_runner runners' do
+          is_expected.to contain_exactly(offline_project_runner, inactive_project_runner)
+        end
+      end
+
+      context 'with "project_runner" and "active_runner" tags as comma-separated string' do
+        let(:tag_list) { ['project_runner,active_runner'] }
+
+        it 'returns the offline_project_runner runner' do
+          is_expected.to contain_exactly(offline_project_runner)
+        end
+      end
+
+      context 'with "active_runner" and "instance_runner" tags as array' do
+        let(:tag_list) { %w[instance_runner active_runner] }
+
+        it 'returns the instance_runner runner' do
+          is_expected.to contain_exactly(instance_runner)
+        end
+      end
+    end
+
+    context 'when text is filtered' do
+      let(:args) do
+        { search: search_term }
+      end
+
+      context 'to "project"' do
+        let(:search_term) { 'project' }
+
+        it 'returns both project runners' do
+          is_expected.to contain_exactly(inactive_project_runner, offline_project_runner)
+        end
+      end
+
+      context 'to "group"' do
+        let(:search_term) { 'group' }
+
+        it 'returns group runners' do
+          is_expected.to contain_exactly(group_runner, subgroup_runner)
+        end
+      end
+
+      context 'to "defghi"' do
+        let(:search_term) { 'defghi' }
+
+        it 'returns runners containing term in token' do
+          is_expected.to contain_exactly(offline_project_runner)
+        end
       end
     end
   end
