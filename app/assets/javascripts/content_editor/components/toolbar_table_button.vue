3c591a65c92882d2853b5985a4e55df1c8749935
Himanshu Kapoor <hkapoor@gitlab.com>
2021-08-13 22:50:46 +0200

    Allow editing table structure in content editor
    Now you can edit the structure of a table, for instance, adding
    or removing rows or columns, toggling header row, or removing
    the table altogether.
    
    Changelog: added

--- app/assets/javascripts/content_editor/components/toolbar_table_button.vue
+++ app/assets/javascripts/content_editor/components/toolbar_table_button.vue
@@ -1,9 +1,11 @@
 <script>
-import { GlDropdown, GlDropdownDivider, GlDropdownForm } from '@gitlab/ui';
+import { GlDropdown, GlDropdownItem, GlDropdownDivider, GlDropdownForm } from '@gitlab/ui';
+import { selectedRect as getSelectedRect } from 'prosemirror-tables';
 import { __, sprintf } from '~/locale';
+import Table from '../extensions/table';
+import TableHeader from '../extensions/table_header';
 import { clamp } from '../services/utils';
-
-export const tableContentType = 'table';
+import EditorStateObserver from './editor_state_observer.vue';
 
 const MIN_ROWS = 3;
 const MIN_COLS = 3;
@@ -13,8 +15,10 @@ const MAX_COLS = 8;
 export default {
   components: {
     GlDropdown,
+    GlDropdownItem,
     GlDropdownDivider,
     GlDropdownForm,
+    EditorStateObserver,
   },
   inject: ['tiptapEditor'],
   data() {
@@ -23,11 +27,36 @@ export default {
       maxCols: MIN_COLS,
       rows: 1,
       cols: 1,
+      isActive: false,
+      isHeaderActive: false,
+      selectedRect: null,
     };
+  },
+  computed: {
+    isFirstRow() {
+      return this.selectedRect?.top === 0;
+    },
+    totalRows() {
+      return this.selectedRect?.map.height;
+    },
+    totalCols() {
+      return this.selectedRect?.map.width;
+    },
+    selectedRows() {
+      return Math.abs(this.selectedRect?.top - this.selectedRect?.bottom);
+    },
+    selectedCols() {
+      return Math.abs(this.selectedRect?.left - this.selectedRect?.right);
+    },
   },
   methods: {
     list(n) {
       return new Array(n).fill().map((_, i) => i + 1);
+    },
+    updateTableState({ editor }) {
+      this.isActive = editor.isActive(Table.name);
+      this.isHeaderActive = editor.isActive(TableHeader.name);
+      if (this.isActive) this.selectedRect = getSelectedRect(editor.state);
     },
     setRowsAndCols(rows, cols) {
       this.rows = rows;
@@ -40,19 +69,13 @@ export default {
       this.cols = 1;
     },
     insertTable() {
-      this.tiptapEditor
-        .chain()
-        .focus()
-        .insertTable({
-          rows: this.rows,
-          cols: this.cols,
-          withHeaderRow: true,
-        })
-        .run();
-
+      this.runCommand('insertTable', { rows: this.rows, cols: this.cols, withHeaderRow: true });
       this.resetState();
 
-      this.$emit('execute', { contentType: 'table' });
+      this.$emit('execute', { contentType: Table.name });
+    },
+    runCommand(name, params = {}) {
+      this.tiptapEditor.chain().focus()[name](params).run();
     },
     getButtonLabel(rows, cols) {
       return sprintf(__('Insert a %{rows}x%{cols} table.'), { rows, cols });
@@ -61,24 +84,61 @@ export default {
 };
 </script>
 <template>
-  <gl-dropdown size="small" category="tertiary" icon="table" class="table-dropdown">
-    <gl-dropdown-form class="gl-px-3!">
-      <div v-for="r of list(maxRows)" :key="r" class="gl-display-flex">
-        <div
-          v-for="c of list(maxCols)"
-          :key="c"
-          :data-testid="`table-${r}-${c}`"
-          :class="{ active: r <= rows && c <= cols }"
-          :aria-label="getButtonLabel(r, c)"
-          class="table-creator-grid-item js-table-creator-grid-item"
-          @mouseover="setRowsAndCols(r, c)"
-          @click="insertTable()"
-        ></div>
-      </div>
-      <gl-dropdown-divider class="gl-my-3! gl-mx-n4!" />
-      <div class="gl-px-1">
-        {{ getButtonLabel(rows, cols) }}
-      </div>
-    </gl-dropdown-form>
-  </gl-dropdown>
+  <editor-state-observer @transaction="updateTableState">
+    <gl-dropdown
+      :toggle-class="{ active: isActive }"
+      size="small"
+      category="tertiary"
+      icon="table"
+      class="table-dropdown"
+      @mousedown.prevent=""
+      @click.prevent=""
+    >
+      <gl-dropdown-form v-if="!isActive" class="gl-px-3!">
+        <div v-for="r of list(maxRows)" :key="r" class="gl-display-flex">
+          <div
+            v-for="c of list(maxCols)"
+            :key="c"
+            :data-testid="`table-${r}-${c}`"
+            :class="{ active: r <= rows && c <= cols }"
+            :aria-label="getButtonLabel(r, c)"
+            class="table-creator-grid-item js-table-creator-grid-item"
+            @mouseover="setRowsAndCols(r, c)"
+            @click="insertTable()"
+          ></div>
+        </div>
+        <gl-dropdown-divider class="gl-my-3! gl-mx-n4!" />
+        <div class="gl-px-1">
+          {{ getButtonLabel(rows, cols) }}
+        </div>
+      </gl-dropdown-form>
+      <template v-if="isActive">
+        <gl-dropdown-item @click="runCommand('addColumnBefore')">
+          {{ __('Insert column before') }}
+        </gl-dropdown-item>
+        <gl-dropdown-item @click="runCommand('addColumnAfter')">
+          {{ __('Insert column after') }}
+        </gl-dropdown-item>
+        <gl-dropdown-item v-if="!isHeaderActive" @click="runCommand('addRowBefore')">
+          {{ __('Insert row before') }}
+        </gl-dropdown-item>
+        <gl-dropdown-item @click="runCommand('addRowAfter')">
+          {{ __('Insert row after') }}
+        </gl-dropdown-item>
+        <gl-dropdown-item v-if="isFirstRow" @click="runCommand('toggleHeaderRow')">
+          {{ __('Toggle header row') }}
+        </gl-dropdown-item>
+        <gl-dropdown-divider />
+        <gl-dropdown-item v-if="totalRows > 1" @click="runCommand('deleteRow')">
+          {{ n__('Delete row', 'Delete %d rows', selectedRows) }}
+        </gl-dropdown-item>
+        <gl-dropdown-item v-if="totalCols > 1" @click="runCommand('deleteColumn')">
+          {{ n__('Delete column', 'Delete %d columns', selectedCols) }}
+        </gl-dropdown-item>
+        <gl-dropdown-item @click="runCommand('deleteTable')">
+          {{ __('Delete table') }}
+        </gl-dropdown-item>
+      </template>
+    </gl-dropdown>
+  </editor-state-observer>
 </template>
