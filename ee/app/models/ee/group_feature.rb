4c4abff074e58118e20da4ebade22fd7969f9735
Francisco Javier López <fjlopez@gitlab.com>
2021-08-23 15:19:02 +0200

    Change class namespace

---
+++ ee/app/models/ee/group_feature.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module EE
+  module GroupFeature
+    extend ActiveSupport::Concern
+
+    EE_FEATURES = %i(wiki).freeze
+
+    prepended do
+      set_available_features(EE_FEATURES)
+
+      default_value_for :wiki_access_level, value: Featurable::ENABLED, allows_nil: false
+    end
+  end
+end
