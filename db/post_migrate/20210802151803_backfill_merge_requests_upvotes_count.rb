074746b2db209f995ad7fdce8d2b0414f41cd746
Terri Chu <tchu@gitlab.com>
2021-08-25 14:22:22 -0400

    Use AwardEmoji table only to query background migration data

--- db/post_migrate/20210802151803_backfill_merge_requests_upvotes_count.rb
+++ db/post_migrate/20210802151803_backfill_merge_requests_upvotes_count.rb
@@ -9,14 +9,21 @@ class BackfillMergeRequestsUpvotesCount < ActiveRecord::Migration[6.1]
   DELAY_INTERVAL = 2.minutes
   BATCH_SIZE = 5_000
 
+  class AwardEmoji < ActiveRecord::Base
+    include EachBatch
+
+    self.table_name = 'award_emoji'
+  end
+
   def up
-    scope = MergeRequest.joins("INNER JOIN award_emoji e ON e.awardable_id = merge_requests.id AND e.awardable_type = 'MergeRequest' AND e.name = 'thumbsup'")
+    scope = AwardEmoji.where(awardable_type: 'MergeRequest').where(name: 'thumbsup')
 
     queue_background_migration_jobs_by_range_at_intervals(
       scope,
       MIGRATION,
       DELAY_INTERVAL,
-      batch_size: BATCH_SIZE
+      batch_size: BATCH_SIZE,
+      primary_column_name: :awardable_id
     )
   end
 
