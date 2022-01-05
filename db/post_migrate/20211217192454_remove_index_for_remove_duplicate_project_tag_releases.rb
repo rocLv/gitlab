6cf85da3420d5948798cc15994df2ab076a7fd59
Allen Cook <acook@gitlab.com>
2022-01-05 11:20:40 -0500

    Add index for migration to speed up deletion of duplicate releases
    Add migration for adding and removing index around existing migration

---
+++ db/post_migrate/20211217192454_remove_index_for_remove_duplicate_project_tag_releases.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class RemoveIndexForRemoveDuplicateProjectTagReleases < Gitlab::Database::Migration[1.0]
+  include Gitlab::Database::MigrationHelpers
+
+  DOWNTIME = false
+  INDEX_NAME = 'index_releases_on_project_rag_released_at'
+
+  disable_ddl_transaction!
+
+  def up
+    remove_concurrent_index_by_name :releases, name: INDEX_NAME
+  end
+
+  def down
+    add_concurrent_index :releases,
+                         %i[project_id tag released_at],
+                         name: INDEX_NAME
+  end
+end
